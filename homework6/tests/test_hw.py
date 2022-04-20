888d5c1f82ab9d3ea7cb280807985b19828a3c0e
Que <quemitariousa6@gmail.com>
2022-04-21 00:36:23 +0300

    add tests

---
+++ homework6/tests/test_hw.py
@@ -0,0 +1,36 @@
+import pytest
+
+from homework6.mysql.builder import MysqlBuilder
+from homework6.mysql.client import MysqlClient
+from homework6.mysql.models import *
+
+
+class SQLBase:
+
+    @pytest.fixture(scope='function', autouse=True)
+    def setup(self, mysql_client):
+        self.mysql: MysqlClient = mysql_client
+        self.builder: MysqlBuilder = MysqlBuilder(self.mysql)
+
+
+class Test_SQL(SQLBase):
+
+    def test_count(self):
+        self.builder.total_count()
+        assert 1 == len(self.mysql.session.query(TotalCountModel).all())
+
+    def test_total_number_of_request_by_type(self):
+        self.builder.method_count()
+        assert 5 == len(self.mysql.session.query(TotalNumberOfRequestsByTypeModel).all())
+
+    def test_top_10_most_frequent_requests(self):
+        self.builder.top_10()
+        assert 10 == len(self.mysql.session.query(Top10MostFrequentRequestsModel).all())
+
+    def test_top_client_errors(self):
+        self.builder.top_4xx()
+        assert 5 == len(self.mysql.session.query(TopClientErrorsModel).all())
+
+    def test_top_server_errors(self):
+        self.builder.top_5xx()
+        assert 5 == len(self.mysql.session.query(TopServerErrorsModel).all())
